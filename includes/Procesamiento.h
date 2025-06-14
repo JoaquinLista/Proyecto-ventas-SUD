d05d706ee9645e71bf4db41bd97e8a6bd8e48e8f
Joaquin Lista <116822768+JoaquinLista@users.noreply.github.com>
2025-06-13 23:04:13 -0300

    Update Procesamiento.h

--- includes/Procesamiento.h
+++ includes/Procesamiento.h
@@ -1,13 +1,16 @@
-
 #ifndef PROCESAMIENTO_H
 #define PROCESAMIENTO_H
 
+#include "Ventas.h"
+#include <vector>
+/**
+ * Calcula e imprime estadísticas generales sobre las ventas.
+ * Muestra métricas como el total de ventas, promedio por categoría,
+ * productos más y menos vendidos, medios de envío más usados,
+ * y otros datos relevantes.
+ *
+ * @param ventas Vector que contiene todas las ventas cargadas.
+ */
+void calcular_estadisticas(const std::vector<Venta> &ventas);
 
-
-class Procesamiento {
-
-};
-
-
-
-#endif //PROCESAMIENTO_H
+#endif // PROCESAMIENTO_H
